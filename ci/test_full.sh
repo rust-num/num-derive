d81b72267c260e8bfcc76c7f6af57aeeff88c8e3
Josh Stone <cuviper@gmail.com>
2020-04-20 15:13:59 -0700

    Switch CI to GitHub Actions

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-echo Testing num-derive on rustc ${TRAVIS_RUST_VERSION}
+CRATE=num-derive
+MSRV=1.31
 
-# num-derive should build and test everywhere.
-cargo build --verbose --features="$FEATURES"
-cargo test --verbose --features="$FEATURES"
+get_rust_version() {
+  local array=($(rustc --version));
+  echo "${array[1]}";
+  return 0;
+}
+RUST_VERSION=$(get_rust_version)
+
+check_version() {
+  IFS=. read -ra rust <<< "$RUST_VERSION"
+  IFS=. read -ra want <<< "$1"
+  [[ "${rust[0]}" -gt "${want[0]}" ||
+   ( "${rust[0]}" -eq "${want[0]}" &&
+     "${rust[1]}" -ge "${want[1]}" )
+  ]]
+}
+
+echo "Testing $CRATE on rustc $RUST_VERSION"
+if ! check_version $MSRV ; then
+  echo "The minimum for $CRATE is rustc $MSRV"
+  exit 1
+fi
+
+FEATURES=(full-syntax)
+echo "Testing supported features: ${FEATURES[*]}"
+
+set -x
+
+# test the default
+cargo build
+cargo test
+
+# test each isolated feature
+for feature in ${FEATURES[*]}; do
+  cargo build --no-default-features --features="$feature"
+  cargo test --no-default-features --features="$feature"
+done
+
+# test all supported features
+cargo build --features="${FEATURES[*]}"
+cargo test --features="${FEATURES[*]}"
